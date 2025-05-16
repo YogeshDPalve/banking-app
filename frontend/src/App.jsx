1adb89f1714b2abb8e11e07a879aca6cc47fd569
Yogesh Palve <dnyyogesh@gmail.com>
2025-05-16 22:23:44 +0530

    Full file setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-rose-500 font-bold ">
+        Hello this is banking app
+      </h1>
+    </>
+  );
+};
+
+export default App;
